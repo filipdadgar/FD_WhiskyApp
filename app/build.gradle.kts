14214947420fce5f87e87da3207cac532c829df7
Filip Dadgar <filip.dadgar.ext@t-hive.io>
2025-09-21 16:38:57 +0200

    update enabling viewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         compose = false
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/filipdadgar/fd_whiskyapp/MainActivity.kt
+++ app/src/main/java/com/filipdadgar/fd_whiskyapp/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.filipdadgar.fd_whiskyapp
 
 import android.os.Bundle
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.ImageView
 import android.widget.ArrayAdapter
 import android.view.View
 import android.widget.AdapterView
+import com.filipdadgar.fd_whiskyapp.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsCompat.Type
 
 class MainActivity : AppCompatActivity() {
+	private lateinit var binding: ActivityMainBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.resultTextView)) { v, insets ->
+		ViewCompat.setOnApplyWindowInsetsListener(binding.resultTextView) { v, insets ->
 			val systemBars = insets.getInsets(Type.systemBars())
 			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 			insets
@@ -28,26 +29,19 @@ class MainActivity : AppCompatActivity() {
 		val artists = resources.getStringArray(R.array.artists)
 		val sizes = resources.getStringArray(R.array.sizes)
 
-		val resultTextView = findViewById<TextView>(R.id.resultTextView)
-		val whiskySpinner: Spinner = findViewById(R.id.whiskySpinner)
-		val artistSpinner: Spinner = findViewById(R.id.artistSpinner)
-		val sizeSpinner: Spinner = findViewById(R.id.sizeSpinner)
-		val whiskyImageView: ImageView = findViewById(R.id.whiskyImageView)
-		val artistImageView: ImageView = findViewById(R.id.artistImageView)
-
 		val whiskyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, whiskies)
 		whiskyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-		whiskySpinner.adapter = whiskyAdapter
+		binding.whiskySpinner.adapter = whiskyAdapter
 
 		val artistAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, artists)
 		artistAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-		artistSpinner.adapter = artistAdapter
+		binding.artistSpinner.adapter = artistAdapter
 
 		val sizeAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, sizes)
 		sizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-		sizeSpinner.adapter = sizeAdapter
+		binding.sizeSpinner.adapter = sizeAdapter
 
-		fun printInfo(tView: TextView, whiskyIndex: Int, artistIndex: Int, sizeIndex: Int) {
+		fun printInfo(whiskyIndex: Int, artistIndex: Int, sizeIndex: Int) {
 			val whiskies = resources.getStringArray(R.array.whiskies)
 			val prices = resources.getStringArray(R.array.prices)
 			val artists = resources.getStringArray(R.array.artists)
@@ -58,37 +52,37 @@ class MainActivity : AppCompatActivity() {
 				val artist = artists[artistIndex]
 				val sizeCl = sizes[sizeIndex].toInt()
 				val totalPrice = pricePerCl * sizeCl
-				tView.text = "Du har valt $whisky.\nPris per cl: $pricePerCl kr.\nStorlek: $sizeCl cl.\nTotalpris: $totalPrice kr.\nArtist: $artist."
+				binding.resultTextView.text = "Du har valt $whisky.\nPris per cl: $pricePerCl kr.\nStorlek: $sizeCl cl.\nTotalpris: $totalPrice kr.\nArtist: $artist."
 				// Set images if available
 				val whiskyImageRes = resources.getIdentifier("whisky_${whiskyIndex}", "drawable", packageName)
-				if (whiskyImageRes != 0) whiskyImageView.setImageResource(whiskyImageRes)
-				else whiskyImageView.setImageDrawable(null)
+				if (whiskyImageRes != 0) binding.whiskyImageView.setImageResource(whiskyImageRes)
+				else binding.whiskyImageView.setImageDrawable(null)
 				val artistImageRes = resources.getIdentifier("artist_${artistIndex}", "drawable", packageName)
-				if (artistImageRes != 0) artistImageView.setImageResource(artistImageRes)
-				else artistImageView.setImageDrawable(null)
+				if (artistImageRes != 0) binding.artistImageView.setImageResource(artistImageRes)
+				else binding.artistImageView.setImageDrawable(null)
 			} else {
-				tView.text = getString(R.string.printText)
-				whiskyImageView.setImageDrawable(null)
-				artistImageView.setImageDrawable(null)
+				binding.resultTextView.text = getString(R.string.printText)
+				binding.whiskyImageView.setImageDrawable(null)
+				binding.artistImageView.setImageDrawable(null)
 			}
 		}
 
-		whiskySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+		binding.whiskySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 			override fun onNothingSelected(parent: AdapterView<*>?) {}
 			override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-				printInfo(resultTextView, position, artistSpinner.selectedItemPosition, sizeSpinner.selectedItemPosition)
+				printInfo(position, binding.artistSpinner.selectedItemPosition, binding.sizeSpinner.selectedItemPosition)
 			}
 		}
-		artistSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+		binding.artistSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 			override fun onNothingSelected(parent: AdapterView<*>?) {}
 			override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-				printInfo(resultTextView, whiskySpinner.selectedItemPosition, position, sizeSpinner.selectedItemPosition)
+				printInfo(binding.whiskySpinner.selectedItemPosition, position, binding.sizeSpinner.selectedItemPosition)
 			}
 		}
-		sizeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+		binding.sizeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 			override fun onNothingSelected(parent: AdapterView<*>?) {}
 			override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-				printInfo(resultTextView, whiskySpinner.selectedItemPosition, artistSpinner.selectedItemPosition, position)
+				printInfo(binding.whiskySpinner.selectedItemPosition, binding.artistSpinner.selectedItemPosition, position)
 			}
 		}
 	}
